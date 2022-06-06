c5a2f535fb0a3ff9c6b99a1d102d5c2ad27d655f
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2022-06-06 11:58:11 +0300

    simple commit

---
+++ vendor/composer/autoload_real.php
@@ -0,0 +1,26 @@
+<?php
+
+class ComposerAutoloaderInit12bb6664b5db9219c96655c8e2c258fc
+{
+    private static $loader;
+
+    public static function loadClassLoader($class)
+    {
+        if ('Composer\Autoload\ClassLoader' === $class) {
+            require __DIR__ . '/ClassLoader.php';
+        }
+    }
+
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
+    public static function getLoader()
+    {
+        if (null !== self::$loader) {
+            return self::$loader;
+        }
+
+        
+    }
+
+}
--- web/index.php
+++ web/index.php
@@ -1,3 +1,10 @@
 <?php
 
+ini_set('display_errors', E_ALL);
+
 // comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../vendor/autoload.php';
+
